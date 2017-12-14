8f33e484bd6eac950d95ae67ac4405be27df22f0
Chris Shaw <chris@shawcando.com>
2017-12-14 13:07:16 +0200

    Created the customer model

--- sb_project/account/models.py
+++ sb_project/account/models.py
@@ -3,24 +3,26 @@ from django.contrib.auth.models import User
 
 from datetime import datetime
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/customer_<id>/<filename>
+    return 'customer_{0}/{1}'.format(instance.customer.id, filename)
 
 class customer(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    id_number = models.CharField(max_length=32)
-    phone_number = models.CharField(max_length=32)
-    address = models.TextField(max_length=100, blank=True)
-    headshot_image = models.ImageField('profile picture',
-                               upload_to='avatars/',
+    user_name = models.CharField(max_length=16)
+    first_name = models.CharField(max_length=16)
+    last_name = models.CharField(max_length=16)
+    id_number = models.CharField(max_length=16)
+    phone_number = models.CharField(max_length=16)
+    email_address = models.EmailField()
+    physical_address = models.TextField(max_length=128, blank=True)
+    headshot_image = models.ImageField('Customer Photo',
+                               upload_to=user_directory_path,
                                null=True, blank=True)
-    residence_image = models.ImageField('profile picture',
-                              upload_to='avatars/',
+    residence_image = models.ImageField('Proof of residence',
+                              upload_to=user_directory_path,
                               null=True, blank=True)
-    signature_image = models.ImageField('profile picture',
-                              upload_to='avatars/',
+    signature_image = models.ImageField('Customer Signature',
+                              upload_to=user_directory_path,
                               null=True, blank=True)
-    created_by = models.OneToOneField(User, on_delete=models.CASCADE,
-                              related_name='customer')
+    created_by = models.ForeignKey(User, related_name='customers')
     created_date = models.DateTimeField(default=datetime.now, blank=True)
-
-# Todo, create folder for user id for Images
