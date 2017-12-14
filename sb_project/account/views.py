3f6f14c95cf1b4a5b8c8040b953a1124f705a072
Christopher J Shaw <chris@shawcando.com>
2017-12-14 23:11:02 +0200

    Added List Customers

--- sb_project/account/views.py
+++ sb_project/account/views.py
@@ -1,10 +1,25 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
 
 from .forms import AccountForm
+from .models import Customer
 
 # List all customers accounts
 def listAccounts(request):
-    return render(request, 'start.html', {})
+    customer_list = Customer.objects.order_by('user_name')
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(customer_list, 10)
+
+    try:
+        customers = paginator.page(page)
+    except PageNotAnInteger:
+        customers = paginator.page(1)
+    except EmptyPage:
+        customers = paginator.page(paginator.num_pages)
+
+    return render(request, 'account_list.html', {'customers': customers})
+
 
 # Create a new customer account
 def newAccount(request):
@@ -15,13 +30,16 @@ def newAccount(request):
             account = form.save(commit=False)
             account.created_by = request.user
             account.save()
+            # Todo, send sms here
             return redirect('view_account', user_name=account.user_name)
 
     return render(request, 'account_edit.html', {'form': form})
 
+
 # View a customer account
 def viewAccount(request, user_name):
     return render(request, 'start.html', {})
+
 
 # Edit a customer account
 def editAccount(request, user_name):
