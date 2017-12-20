99882e15dbbf288a38e1bccf3db4d1da8c8126f4
Christopher J Shaw <chris@shawcando.com>
2017-12-20 22:57:50 +0200

    Started adding filters on backend

--- sb_project/backend/views.py
+++ sb_project/backend/views.py
@@ -1,12 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
-from account.models import AccessLog
+from account.models import AccessLog, Customer
 
 @login_required
 def backend(request):
+    # Get gata
+    customers = Customer.objects.order_by('last_name', 'first_name')
+    users = User.objects.order_by('last_name', 'first_name')
     log_list = AccessLog.objects.order_by('-timestamp')
     page = request.GET.get('page', 1)
 
@@ -19,4 +23,12 @@ def backend(request):
     except EmptyPage:
         log = paginator.page(paginator.num_pages)
 
-    return render(request, 'backend.html', {'log': log})
+    # Log View
+    AccessLog.objects.create(user=request.user,
+                             level=AccessLog.INFO,
+                             notes="Viewed backend, page: {}".format(page))
+
+    return render(request, 'backend.html', {'log': log,
+                                            'customers': customers,
+                                            'users': users,
+                                            })
